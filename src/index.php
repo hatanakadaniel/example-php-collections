4f6227b00779bf9b92720a5c41bae6f49e9beb57
Daniel Hatanaka <daniel.hatanaka@s2it.com.br>
2018-06-07 15:49:06 -0300

    update show information

--- src/index.php
+++ src/index.php
@@ -9,44 +9,44 @@ ini_set("max_execution_time", 3600);
 $count = 8;
 $realUsed = false;
 
-echo memory_get_usage($realUsed) . "<br>" . PHP_EOL;
+print 'Memory: ' . memory_get_usage($realUsed) . ' bytes' . PHP_EOL;
 $vector = new \Ds\Deque();
 $initTime = microtime(true);
 for ($i=0; $i < 10**$count; $i++) {
     $vector->push('a');
-//    echo memory_get_usage($realUsed) . "<br>" . PHP_EOL;
+//    print 'Memory: ' . memory_get_usage($realUsed) . ' bytes' . PHP_EOL;
 }
 $finalTime = microtime(true);
-echo memory_get_usage($realUsed) . "<br>" . PHP_EOL;
-print_r($finalTime - $initTime);
+print 'Memory: ' . memory_get_usage($realUsed) . ' bytes' . PHP_EOL;
+print 'Time: ' . ($finalTime - $initTime) . PHP_EOL;
 
 
-echo  "<br><br>" . PHP_EOL . PHP_EOL;
+echo PHP_EOL . PHP_EOL;
 
 
 unset($vector);
-echo memory_get_usage($realUsed) . "<br>" . PHP_EOL;
+print 'Memory: ' . memory_get_usage($realUsed) . ' bytes' . PHP_EOL;
 $vector = new SplDoublyLinkedList();
 $initTime = microtime(true);
 for ($i=0; $i < 10**$count; $i++) {
     $vector->push('a');
-//    echo memory_get_usage($realUsed) . "<br>" . PHP_EOL;
+//    print 'Memory: ' . memory_get_usage($realUsed) . ' bytes' . PHP_EOL;
 }
 $finalTime = microtime(true);
-echo memory_get_usage($realUsed) . "<br>" . PHP_EOL;
-print_r($finalTime - $initTime);
+print 'Memory: ' . memory_get_usage($realUsed) . ' bytes' . PHP_EOL;
+print 'Time: ' . ($finalTime - $initTime) . PHP_EOL;
 
 
-echo  "<br><br>\n\n";
+echo PHP_EOL . PHP_EOL;
 
 
 unset($vector);
-echo memory_get_usage($realUsed) . "<br>" . PHP_EOL;
+print 'Memory: ' . memory_get_usage($realUsed) . ' bytes' . PHP_EOL;
 $vector = [];
 $initTime = microtime(true);
 for ($i=0; $i < 10**$count; $i++) {
     $vector[] = 'a';
 }
 $finalTime = microtime(true);
-echo memory_get_usage($realUsed) . "<br>" . PHP_EOL;
-print_r($finalTime - $initTime);
+print 'Memory: ' . memory_get_usage($realUsed) . ' bytes' . PHP_EOL;
+print 'Time: ' . ($finalTime - $initTime) . PHP_EOL;
